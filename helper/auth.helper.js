2aa74688294a11e2dd3e3a7b2b769e25fe4ca7d3
suzangyawali <sujangyawali17@gmail.com>
2025-08-19 23:57:03 +0530

    Remove duplicate backend folders from root; all backend code now in Backend/

--- helper/auth.helper.js
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken"
-const createAuthToken=(data)=>{
-const token=jwt.sign(data,process.env.JWT_SECRET,{
-    expiresIn:86400, // 1 day
-});
-return token;
-}
-export {createAuthToken};
